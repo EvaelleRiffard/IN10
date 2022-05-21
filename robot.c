705f5d70dbc6ccc1865a5012447ec52003098116
Evaelle <>
2022-05-21 19:46:33 +0200

    maj

--- robot.c
+++ robot.c
@@ -6,7 +6,7 @@
 #include "maj_dico.h"
 #include "maj_essai.h"
 #include "decriptage.h"
-#define MAX 10140
+
 
  
 
@@ -17,13 +17,13 @@ char* robot(char* mot){
     int C = 0;
     int i = 0;
 
-    do {Dico = maj_dico(mot, COULEURS_M, DICO);
+    do {Dico = maj_dico(mot, COULEURS_M, Dico);
         essai = maj_essai(essai, Dico);
         printf("%s\n", essai);
         COULEURS_M = decriptage(essai, mot);
         C = 0;
         for (int i = 0; i<5; ++i){ C += COULEURS_M[i];}
-        i = i+1}
+        i = i+1;}
     while (i<1000 && C != 0); 
 
     return essai; 
