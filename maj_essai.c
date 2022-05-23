e9b81cde70ab742c3d81ed52563b1d3bd0139d83
Evaelle <>
2022-05-23 22:49:03 +0200

    maj

--- maj_essai.c
+++ maj_essai.c
@@ -4,9 +4,9 @@
 
 char* maj_essai(char* essai, char** Dico){
     if (Dico[0]==essai){
-        return Dico[2];  //On fait en sorte de ne pas retester notre mot
+        return Dico[0];  //On fait en sorte de ne pas retester notre mot
     }
     else{
-        return Dico[0];
+        return Dico[1];
     }
 }
